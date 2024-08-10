2953eb0005b0af42f9bfb15c9eaeba3ee08fe1a5
Li YuShen <lysrain21@163.com>
2024-08-10 15:10:04 +0800

    first_submit

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,45 @@
-import { ethers } from 'ethers';
+import { ethers } from 'ethers'
 
 const Navigation = ({ account, setAccount }) => {
+    const connectHandler = async () => {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const account = ethers.utils.getAddress(accounts[0])
+        setAccount(account);
+    }
 
     return (
         <nav>
+            <div className='nav__brand'>
+                <h1>果壳文创</h1>
+            </div>
 
+            <input
+                type="text"
+                className="nav__search"
+            />
+
+            {account ? (
+                <button
+                    type="button"
+                    className='nav__connect'
+                >
+                    {account.slice(0, 6) + '...' + account.slice(38, 42)}
+                </button>
+            ) : (
+                <button
+                    type="button"
+                    className='nav__connect'
+                    onClick={connectHandler}
+                >
+                    Connect
+                </button>
+            )}
+
+            <ul className='nav__links'>
+                <li><a href="#文具用品">文具用品</a></li>
+                <li><a href="#国科大徽章">国科大徽章</a></li>
+                <li><a href="#生活用品">生活用品</a></li>
+            </ul>
         </nav>
     );
 }
